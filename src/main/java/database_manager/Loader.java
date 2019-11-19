5ce9708d445822e2714863e250936dd7c8d47722
Walid Jabari <walid.jabari@outlook.com>
2019-11-19 20:20:06 +0100

    initial commit

---
+++ src/main/java/database_manager/Loader.java
@@ -0,0 +1,5 @@
+package database_manager;
+
+public class Loader{
+    
+}
---
+++ src/main/java/graphical_user_interface/App.java
@@ -0,0 +1,20 @@
+package graphical_user_interface;
+
+import javax.swing.*;
+
+/**
+ * Hello world!
+ */
+public final class App {
+    private App() {
+    }
+
+    /**
+     * Says hello to the world.
+     * 
+     * @param args The arguments of the program.
+     */
+    public static void main(final String[] args) {
+        System.out.println("Hello World!");
+    }
+}
