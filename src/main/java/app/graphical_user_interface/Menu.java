5856ebd1ade1cf79e8b3bea3e6184c41e1e45862
redoine Lahdiri <redoine.lahdiri@edu.ece.fr>
2019-11-26 23:24:18 +0100

    update

--- src/main/java/app/graphical_user_interface/Menu.java
+++ src/main/java/app/graphical_user_interface/Menu.java
@@ -1,7 +1,6 @@
 package app.graphical_user_interface;
 
-import app.database_manager.Adresse;
-import app.database_manager.Docteur;
+import javax.swing.*;
 
 /**
  * menu
@@ -14,8 +13,7 @@ enum MyEnum {
 public class Menu {
 
     public static void main(String[] args) {
-        Docteur d = new Docteur("alain", "morelle", "07 67 24 84 91", new Adresse(), "généraliste");
-        System.out.println(d);
+        System.out.println("hello world");
     }
 
 }
