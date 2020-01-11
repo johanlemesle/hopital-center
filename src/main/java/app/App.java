470c9953f96759bc6b6daceb4ec7c9748b05ba3b
redoine.lahdiri@edu.ece.fr <redoine.lahdiri@edu.ece.fr>
2020-01-11 19:57:52 +0100

    update

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -34,6 +34,13 @@ public class App {
                                 new Adresse("12", "rue", "de la Bretonne", "78220", "Viroflay"));
                 Patient redoine = new Patient("Lahdiri", "Redoine", "0708070982",
                                 new Adresse("37", "quai", "de grenelle", "75015", "Juvisy sur Marne"));
+                
+                Patient john = new Patient("Dark", "John", "0353426153",
+                                new Adresse("26", "quai", "de grounelle", "75003", "Paris sur Orge"));
+
+                Patient hermione = new Patient("Granger", "Hermione", "0023212322",
+                                new Adresse("21", "allée", "de poudlard", "91200", "Marseille sur Orge"));
+
                 Docteur poulain = new Docteur("Poulain", "Philippe", "0102030405",
                                 new Adresse("10", "Boulevard", "du Général Leclerc", "78140", "Vélizy Villacoublay"),
                                 "Généraliste");
@@ -53,18 +60,23 @@ public class App {
                 Chambre room205 = new Chambre();
 
 
-
+                //On ne peut pas faire deux hospitalisation différente sur un même patient car 
+                // même clé
                 Hospitalisation greffe = new Hospitalisation(walid, urgence, room205, 2);
                 Hospitalisation platre = new Hospitalisation(johan, urgence, room205, 1);
                 Hospitalisation piqure = new Hospitalisation(redoine, urgence, room205, 1);
-                Hospitalisation couture = new Hospitalisation(johan, urgence, room205, 1);
-                Hospitalisation dechirure = new Hospitalisation(walid, urgence, room205, 1);
+                Hospitalisation couture = new Hospitalisation(john, urgence, room205, 1);
+                Hospitalisation dechirure = new Hospitalisation(hermione, urgence, room205, 1);
 
 
 
                 hopital.insert("patient", walid);
                 hopital.insert("patient", johan);
                 hopital.insert("patient", redoine);
+                hopital.insert("patient", john);
+                hopital.insert("patient", hermione);
+
+
 
                 hopital.insert("infirmier", marec);
 
@@ -89,19 +101,23 @@ public class App {
                                 hopital);
 
                 // Récupérer le nom des services
-                
+
                 ArrayList<Pair<String, Object>> intermed1 = Utils.get("services{nom}", hopital);
                 ArrayList<Pair<String, Object>> intermed2 = (ArrayList<Pair<String, Object>>) intermed1.get(0).getRight();
                 ArrayList<Pair<String, Object>> servicesName = (ArrayList<Pair<String, Object>>) intermed2.get(0).getRight();
 
                 //Récupérer le nombre d'hospitalisation dans un service
                 
-                ArrayList<Pair<String, Object>> list = Utils.get("hospitalisations{service{nom}}",hopital);
+                ArrayList<Pair<String, Object>> interm1 = Utils.get("hospitalisations{service{nom}}",hopital);
+                ArrayList<Pair<String, Object>> interm2 = (ArrayList<Pair<String, Object>>) interm1.get(0).getRight();
+                ArrayList<Pair<String, Object>> interm3 = (ArrayList<Pair<String, Object>>) interm2.get(0).getRight();
+                ArrayList<Pair<String, Object>> interm4 = (ArrayList<Pair<String, Object>>) interm3.get(0).getRight();
 
-        
+                //Cette ligne affiche le nom du service de l'hospitalisation numéro 1 get(0)
+                System.out.println(interm4.get(0).getRight());
+
                 // Données : Nom des services   
                 DefaultPieDataset dataset = new DefaultPieDataset();
-                
                 for (int i = 0; i < servicesName.size(); i++)
                 {
                         Random r = new Random();
@@ -109,13 +125,11 @@ public class App {
                         String tst = servicesName.get(i).getRight().toString();
                         dataset.setValue(tst, randomValue);
                 }
-
                 // création de mon chart
                 PieChartTest chart1 = new PieChartTest("JABARI LEMESLE LAHDIRI", dataset);
                 chart1.setSize(560, 360);
                 RefineryUtilities.centerFrameOnScreen(chart1);
                 chart1.setVisible(true);
-
                 System.out.println("done");
 		
                 
