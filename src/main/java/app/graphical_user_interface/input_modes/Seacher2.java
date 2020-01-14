d42517026e4e889367e5d2327105e5c95d946b13
Walid <walid.jabari@outlook.com>
2020-01-14 04:22:35 +0100

    update

--- src/main/java/app/graphical_user_interface/input_modes/Seacher2.java
+++ src/main/java/app/graphical_user_interface/input_modes/Seacher2.java
@@ -1,22 +1,50 @@
 package app.graphical_user_interface.input_modes;
 
+import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTree;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeModel;
 import javax.swing.tree.TreePath;
 
-import app.graphical_user_interface.helpers.JCheckBoxTree;
-import app.graphical_user_interface.helpers.MyDomainCheckBoxTree;
-
 /**
  * Seacher2
  */
-public class Seacher2 {
+public class Seacher2 extends JFrame {
 
-    public Seacher2(JPanel workingPane) {
+    /**
+     *
+     */
+    private static final long serialVersionUID = 7272210466424999777L;
+    private JTree tree;
 
-        MyDomainCheckBoxTree m = new MyDomainCheckBoxTree(false, workingPane);
-        
+    public Seacher2() {
+        // create the root node
+        DefaultMutableTreeNode root = new DefaultMutableTreeNode("Root");
+        // create the child nodes
+        DefaultMutableTreeNode vegetableNode = new DefaultMutableTreeNode("Vegetables");
+        DefaultMutableTreeNode fruitNode = new DefaultMutableTreeNode("Fruits");
+        // add the child nodes to the root node
+        root.add(vegetableNode);
+        root.add(fruitNode);
 
+        // create the tree by passing in the root node
+        tree = new JTree(root);
+        add(tree);
+
+        vegetableNode.add(new DefaultMutableTreeNode("Capsicum"));
+        vegetableNode.add(new DefaultMutableTreeNode("Carrot"));
+        vegetableNode.add(new DefaultMutableTreeNode("Tomato"));
+        vegetableNode.add(new DefaultMutableTreeNode("Potato"));
+        fruitNode.add(new DefaultMutableTreeNode("Banana"));
+        fruitNode.add(new DefaultMutableTreeNode("Mango"));
+        fruitNode.add(new DefaultMutableTreeNode("Apple"));
+        fruitNode.add(new DefaultMutableTreeNode("Grapes"));
+        fruitNode.add(new DefaultMutableTreeNode("Orange"));
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setTitle("JTree Example");
+        this.pack();
+        this.setVisible(true);
     }
+
 }
